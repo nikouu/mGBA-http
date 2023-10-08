6598409f4362d2e4648cd9c402722820a8db8825
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2023-10-08 16:45:27 +1300

    Added test console for manual testing

---
+++ mGBAHttpServer.TestClient/Program.cs
@@ -0,0 +1,39 @@
+﻿var client = new HttpClient();
+
+var keysDictionary = new Dictionary<string, string>()
+{
+    {"X", "A"},
+    {"Z", "B"},
+    {"A", "L"},
+    {"S", "R"},
+    {"Enter", "Start"},
+    {"Backspace", "Select"},
+    {"UpArrow", "Up" },
+    {"RightArrow", "Right" },
+    {"DownArrow", "Down" },
+    {"LeftArrow", "Left" },
+};
+
+Console.Title = "mGBA-http test console";
+Console.WriteLine("Test client to pass keys to mGBA via mGBA-http.");
+Console.WriteLine("Buttons: A:X, B:Z, L:A, R:S, Start:Enter, Select:Backspace, Arrow keys. q to exit");
+
+Console.Write(">>");
+
+while (Console.ReadKey() is ConsoleKeyInfo consoleKeyInfo)
+{
+    var keyString = consoleKeyInfo.Key.ToString();
+
+    if (keysDictionary.ContainsKey(keyString))
+    {
+        await SendKey(keysDictionary[keyString]);
+    }
+}
+
+async Task SendKey(string key)
+{
+    var request = new HttpRequestMessage(HttpMethod.Post, $"https://localhost:7173/button?key={key}");
+    var response = await client.SendAsync(request);
+    response.EnsureSuccessStatusCode();
+    Console.WriteLine(await response.Content.ReadAsStringAsync());
+}
