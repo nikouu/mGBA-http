4cc99a7d68bca8dacf1a732d38e2cae1f21e65d4
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2023-10-09 21:17:13 +1300

    Updated crude publishing script

--- mGBAHttpServer/ReleaseBuild.ps1
+++ mGBAHttpServer/ReleaseBuild.ps1
@@ -1,1 +1,9 @@
-dotnet publish .\mGBAHttpServer.csproj -c Release -r win-x64 -p:PublishSingleFile=true -p:TrimMode=partial -p:DebugType=embedded -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true --self-contained true 
+$version = "0.0.0"
+
+$win64SelfContainedFilename = "mGBA-http-{0}-win64-self-contained" -f $version
+dotnet publish .\mGBAHttpServer.csproj -c Release -f net8.0 -r win-x64 --self-contained true  -p:PublishSingleFile=true -p:TrimMode=partial -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true -p:JsonSerializerIsReflectionEnabledByDefault=true -p:DebuggerSupport=false -p:AssemblyName=$win64SelfContainedFilename
+
+<#
+$win64Filename = "mGBA-http-{0}-win64" -f $version
+dotnet publish .\mGBAHttpServer.csproj -c Release -f net8.0 -r win-x64 --self-contained false  -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -p:DebuggerSupport=false -p:AssemblyName=$win64Filename
+#>
