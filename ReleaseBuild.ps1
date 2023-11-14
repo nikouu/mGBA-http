ffb556e521e236f6a46fbb7b98bb319b00fa6fdb
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2023-11-14 21:15:18 +1300

    Finished release script

--- ReleaseBuild.ps1
+++ ReleaseBuild.ps1
@@ -1,12 +1,21 @@
 $projectFilePath = "src\mGBAHttpServer\mGBAHttpServer.csproj"
 $xml = [xml](Get-Content $projectFilePath)
-
 $version = $xml.Project.PropertyGroup.Version
 
 $filenamePrefix = "mGBA-http-{0}" -f $version
-$rids = @("win-x64")
-#, "linux-x64", "osx-x64"
+$rids = @("win-x64", "linux-x64", "osx-x64")
+
+# Clean folder
+Remove-Item .\release\* -Recurse -Force
+
 foreach ($rid in $rids) {
-    dotnet publish src\mGBAHttpServer\mGBAHttpServer.csproj -p:AssemblyName="$($filenamePrefix)-$($rid)" -p:PublishSingleFile=true --self-contained=false
-    #dotnet publish src\mGBAHttpServer\mGBAHttpServer.csproj -r $rid -p:AssemblyName="$($filenamePrefix)-$($rid)-self-contained" --self-contained=true -p:PublishSingleFile=true -p:TrimMode=partial -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true -p:JsonSerializerIsReflectionEnabledByDefault=true -p:DebuggerSupport=false
+  dotnet publish src\mGBAHttpServer\mGBAHttpServer.csproj -r $rid --self-contained=false -p:PublishSingleFile=true -p:DebugType=None -p:DebugSymbols=false -o .\releaseStaging -p:AssemblyName="$($filenamePrefix)-$($rid)"  
+  Move-Item -Path ".\releaseStaging\*.*" -Destination ".\release" -Force
+
+  dotnet publish src\mGBAHttpServer\mGBAHttpServer.csproj -r $rid --self-contained=true  -p:PublishSingleFile=true -p:DebugType=None -p:DebugSymbols=false -o .\releaseStaging -p:AssemblyName="$($filenamePrefix)-$($rid)-self-contained" -p:TrimMode=partial -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true -p:JsonSerializerIsReflectionEnabledByDefault=true
+  Move-Item -Path ".\releaseStaging\*.*" -Destination ".\release" -Force
 }
+
+Copy-Item -Path ".\mGBASocketServer.lua" -Destination ".\release" -Force
+
+Remove-Item .\releaseStaging -Recurse -Force
